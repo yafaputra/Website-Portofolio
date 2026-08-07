5b12280f54ee3617acb7742229e458f3a135a1ed
Yafa Putra <yafaputra94@gmail.com>
2026-08-07 16:14:42 +0700

    style: Change project preview images to object-contain so screenshots are fully visible

--- my-app/app/(landing)/components/hero/project.jsx
+++ my-app/app/(landing)/components/hero/project.jsx
@@ -122,7 +122,7 @@ function ProjectCard({ project }) {
               alt={project.title}
               fill
               sizes="(min-width: 1024px) 33vw, (min-width: 640px) 50vw, 100vw"
-              className="object-cover transition-transform duration-500 group-hover:scale-105 filter grayscale hover:grayscale-0 brightness-[1.05] hover:brightness-[1.10]"
+              className="object-contain transition-transform duration-500 group-hover:scale-105 filter grayscale hover:grayscale-0 brightness-[1.05] hover:brightness-[1.10]"
             />
           </div>
         ) : (
